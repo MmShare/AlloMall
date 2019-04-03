0491e5215be346f04f2f974ce6377419a6595f76
qingmeng952621 <qingmeng952621@163.com>
2019-04-03 10:34:43 +0800

    提交文件

---
+++ src/main/java/com/example/allomall/AllomallApplication.java
@@ -0,0 +1,13 @@
+package com.example.allomall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllomallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllomallApplication.class, args);
+    }
+
+}
