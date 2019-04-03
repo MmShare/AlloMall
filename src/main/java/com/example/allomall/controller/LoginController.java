26e664c06bc8fae2a8510cc9f58082f01e913144
qingmeng952621 <qingmeng952621@163.com>
2019-04-03 11:21:58 +0800

    修改LoginController

--- src/main/java/com/example/allomall/controller/LoginController.java
+++ src/main/java/com/example/allomall/controller/LoginController.java
@@ -1,4 +1,14 @@
 package com.example.allomall.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+@Controller
 public class LoginController {
+
+
+    public String toIndex(){
+        return "";
+    }
 }
