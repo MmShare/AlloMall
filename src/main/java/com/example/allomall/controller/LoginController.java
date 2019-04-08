f13b77353bcd1b359143135f9fb88e0ef0b72fb4
qingmeng952621 <qingmeng952621@163.com>
2019-04-08 09:29:50 +0800

    Layer JS CSS资源

--- src/main/java/com/example/allomall/controller/LoginController.java
+++ src/main/java/com/example/allomall/controller/LoginController.java
@@ -3,12 +3,18 @@ package com.example.allomall.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping
 @Controller
+@RequestMapping("/")
 public class LoginController {
 
 
-    public String toIndex(){
-        return "";
+    @RequestMapping("/index.html")
+    public String toLogin(){
+        return "index";
+    }
+
+    @RequestMapping("/console.html")
+    public String toConsole(){
+        return "home/console";
     }
 }
