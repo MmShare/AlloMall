696144764ce779c7a4c5262bfd3c7862837830a6
qingmeng952621 <qingmeng952621@163.com>
2019-04-19 08:49:29 +0800

    系统管理的第一部分Java代码

--- src/main/java/com/example/allomall/controller/ProductController.java
+++ src/main/java/com/example/allomall/controller/ProductController.java
@@ -1,9 +1,12 @@
 package com.example.allomall.controller;
 
 
+import com.example.allomall.repostitory.TypeRepostitory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +16,9 @@ public class ProductController {
     private static final Logger log= LoggerFactory.getLogger(HomeController.class);
 
 
+    @Autowired
+    private TypeRepostitory typeRepostitory;
+
     @RequestMapping(value = "/product.html/{typeid}")
     public String toAllProduct(){
         log.info("product页面..................................................");
@@ -20,8 +26,13 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/add.html")
-    public String toAddProduct(){
+    public String toAddProduct(ModelMap map){
         log.info("add Product 页面.................");
+        try {
+        map.put("typeList",typeRepostitory.findAll());
+        }catch (Exception e){
+            log.info("进入添加商品页面时,读取门窗类型发生错误");
+        }
         return "product/product-add";
     }
 
