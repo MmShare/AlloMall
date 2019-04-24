44a306f242352b82cc80092304cdcbabad8e1908
qingmeng <qingmeng952621@163.com>
2019-04-24 11:18:19 +0800

    upload.js的修改

--- src/main/java/com/example/allomall/controller/OrderController.java
+++ src/main/java/com/example/allomall/controller/OrderController.java
@@ -1,8 +1,10 @@
 package com.example.allomall.controller;
 
 
+import com.example.allomall.repostitory.OrderRepostitory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,8 +14,10 @@ public class OrderController {
 
     private static final Logger log= LoggerFactory.getLogger(HomeController.class);
 
+    @Autowired
+    private OrderRepostitory orderRepostitory;
 
-    @RequestMapping(value = "/order/list.html")
+    @RequestMapping(value = "/order/list.html/{typeid}")
     public String toAllOrder(){
         return "order/order-list";
     }
